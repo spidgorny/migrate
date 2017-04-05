0aacbd2c06c517a8b7172e5d37026bdc152ebf69
DEPIDSVY <depidsvy@nintendo.de>
2017-04-05 15:52:00 +0200

    - FIX RisVersion4Ors not being processed correctly

--- src/MigrateClass.php
+++ src/MigrateClass.php
@@ -115,7 +115,7 @@ class Migrate {
 
 	function run() {
 		$cmd = ifsetor($_SERVER['argv'][1], 'index');
-		$res = array_slice($_SERVER['argv'], 1);
+		$res = array_slice($_SERVER['argv'], 2);
 		//echo $cmd, BR;
 		$done = false;
 		foreach ($this->modules as $module) {
